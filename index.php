186e8e68a48da147d83d7e00ff0b066c8be03732
narayan <narayan@opspl.com>
2021-05-13 11:51:40 +0530

    main

---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Narayan Rahul - Web developer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    
+	<meta name="description" content="Narayan Rahul, web developer, freelance, php developer, web developer in goa, php developer in goa, freelancer, freelance web developer in goa." />
+    <link rel="canonical" href="https://narayanrahul.me" />
+    <meta name="author" content="Narayan Rahul">
+	<meta name="keywords" content="freelance jobs in goa, freelance work goa, freelance writing jobs goa, freelance job, freelance content writing jobs, writing jobs online,web developer jobs in goa, ui developer jobs in goa, it in goa, startup goa, internship in goa, it jobs in goa, goa it hub,startup goa jobs, web developer, php developer, curchorem, goa, india, top web development companies in bangalore" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151353393-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-151353393-3');
+	</script>
+
+	<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+	<div id="follower">
+        <div id="circle1"></div>
+        <div id="circle2"></div>
+    </div>
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">	
+	<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" data-stellar-background-ratio="0.5">
+		<div class="overlay">
+			<div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t js-fullheight">
+						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+							<div class="profile-thumb" style="background: url(images/user-3.jpg);"></div>
+							<h1><span>Narayan Rahul</span></h1>
+							<h2><span>Web Developer</span></h2>
+							<p>
+								<ul class="fh5co-social-icons">
+									<li><a href="https://twitter.com/Naru_rawool" target="_blank" alt="Narayan Rahul"><i class="icon-twitter2"></i></a></li>
+									<li><a href="https://www.facebook.com/narayan.rawool.98" target="_blank"  alt="Narayan Rahul"><i class="icon-facebook2"></i></a></li>
+									<li><a href="https://www.linkedin.com/in/narayan-rahul/" target="_blank" alt="Narayan Rahul"><i class="icon-linkedin2"></i></a></li>
+									<li><a href="https://www.instagram.com/naru_9_8/" target="_blank"  alt="Narayan Rahul"><i class="icon-instagram2"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-about" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>About Me</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<ul class="info">
+						<li><span class="first-block">Full Name:</span><span class="second-block">Narayan Rahul</span></li>
+						<li><span class="first-block">Phone:</span><span class="second-block">+ 91 7887614731</span></li>
+						<li><span class="first-block">Email:</span><span class="second-block">rawoolnar98@gmail.com</span></li>
+						<li><span class="first-block">Address:</span><span class="second-block">Curchorem Goa - 403706</span></li>
+					</ul>
+				</div>
+				<div class="col-md-8">
+					<h2>Hello There!</h2>
+					<p>Before talking about qualifications and professional abilities, I would like to introduce myself. I am Narayan Rahul and I am from India. Taking web development as a profession not only fulfils my pocket but also my heart because it has been my passion since my early teenage.</p>
+					<p>I believe that people should do things in which they are good at or in which their heart lays. That’s why I chose web development as my career because I believe I’m good at this and mas I am always honest and I am never afraid of doing hard work.</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="https://twitter.com/Naru_rawool" target="_blank" alt="Narayan Rahul"><i class="icon-twitter2"></i></a></li>
+							<li><a href="https://www.facebook.com/narayan.rawool.98" target="_blank"  alt="Narayan Rahul"><i class="icon-facebook2"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/narayan-rahul/" target="_blank" alt="Narayan Rahul"><i class="icon-linkedin2"></i></a></li>
+							<li><a href="https://www.instagram.com/naru_9_8/" target="_blank"  alt="Narayan Rahul"><i class="icon-instagram2"></i></a></li>
+							<li><a href="./assets/resume-narayan-rahul.pdf"><i class="icon-download"></i> Resume</a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-resume" class="fh5co-bg-color">
+		<div id='stars'></div>
+		<div id='stars2'></div>
+		<div id='stars3'></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>My Resume</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0">
+					<ul class="timeline">
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Work Experience</h3></div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Web Developer [Full-Time]</h3>
+									<span class="company">Goa Web Tech - Dec 2018 - Current</span>
+								</div>
+								<div class="timeline-body">
+									<p>Designing & developing websites, managing & testing websites. Developing Api's. Wordpress theme development. Custom websites and system Development.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Web Developer (Intern)</h3>
+									<span class="company">National Informatics Center - Oct 2017 - Dec 2017</span>
+								</div>
+								<div class="timeline-body">
+									<p>Designing & developing websites, managing & testing websites. Server management & networking.</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Teaching & Data Entry Operator [Intern]</h3>
+									<span class="company">Digicom - Apr 2015 - May 2015</span>
+								</div>
+								<div class="timeline-body">
+									<p>Teaching Microsoft Office and other windows application, Data entry.</p>
+								</div>
+							</div>
+						</li>
+
+						<br>
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Education</h3></div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Diploma [Computer Engineering]</h3>
+									<span class="company">Government Polytechnic Curchorem - 2016 - 2018</span>
+								</div>
+								<div class="timeline-body">
+									<p> Java, PHP, C, Software development, Web development, Embedded system, Math LAB, Databse, Internet Security, etc.</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">HSSC [Computer Techniques]</h3>
+									<span class="company">Gaurdian Angel Higher Secondary School - 2014 - 2016</span>
+								</div>
+								<div class="timeline-body">
+									<p>Java, Web Development, Algorithms & flowcharts, Photoshop, Adobe Flash, Linux, Windows, Networking, etc.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">SCC [General]</h3>
+									<span class="company">The New Educational Institute - 2008 - 2014</span>
+								</div>
+								<div class="timeline-body">
+									<p>General studies.</p>
+								</div>
+							</div>
+						</li>
+			    	</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-skills" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Skills</h2>
+				</div>
+			</div>
+			<div class="row row-pb-md">
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-color="#000" data-percent="95"><span><strong>HTML5</strong>95%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="93"><span><strong>CSS3</strong>93%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>JavaScript</strong>90%</span></div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="89"><span><strong>PHP</strong>89%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>MySQL</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>Wordpress</strong>90%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>Codeigniter</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>Laravel</strong>90%</span></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-work" class="fh5co-bg-dark">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Work</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="https://fuoficios.com.ar/" alt="fuoficios" target="_blank" class="work" style="background-image: url(images/fuo.png);">
+						<div class="desc">
+							<h3>FUO (University)</h3>
+							<span>Website + University Management system + App</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="http://latinawebtech.com/" alt="Latina web tech" target="_blank" class="work" style="background-image: url(images/latinawebtech.png);">
+						<div class="desc">
+							<h3>Latina Web Tech</h3>
+							<span>Wordpress Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="https://besmartgoa.com/" alt="Besmartgoa" target="_blank" class="work" style="background-image: url(images/besmart.png);">
+						<div class="desc">
+							<h3>Besmart</h3>
+							<span>Website + System + App</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="Edmonton Royal" target="_blank" class="work" style="background-image: url(images/edmonton.png);">
+						<div class="desc">
+							<h3>Edmonton Royal</h3>
+							<span>Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="Rich Fit Tips" target="_blank" class="work" style="background-image: url(images/richfittips.png);">
+						<div class="desc">
+							<h3>Rich Fit Tips</h3>
+							<span>Ecommerce Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="All Travel Accessories" target="_blank" class="work" style="background-image: url(images/alltravel.png);">
+						<div class="desc">
+							<h3>All Travel Accessories</h3>
+							<span>Ecommerce Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="Goan Homes" target="_blank" class="work" style="background-image: url(images/goanhomes.png);">
+						<div class="desc">
+							<h3>Goan Homes</h3>
+							<span>Realestate Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="MD Realestate" target="_blank" class="work" style="background-image: url(images/mdreal.png);">
+						<div class="desc">
+							<h3>MD Realestate</h3>
+							<span>Realestate Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="Closet" target="_blank" class="work" style="background-image: url(images/closset.png);">
+						<div class="desc">
+							<h3>Closets</h3>
+							<span>Website</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 text-center col-padding animate-box">
+					<a href="#" alt="Jessy Fit" target="_blank" class="work" style="background-image: url(images/jessyfit.png);">
+						<div class="desc">
+							<h3>Jessy Fit</h3>
+							<span>Website</span>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div id="fh5co-started" class="fh5co-bg-dark">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Hire Me!</h2>
+					<p>I am enthusiastic about learning with keen interest, adaptable to work in a competitive environment with quick learning.</p>
+					<p><a href="#contact" class="btn btn-default btn-lg">Contact Me</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="fh5co-consult">
+		<div class="video fh5co-video" style="background-image: url(images/contact.jpg);">
+			<div class="overlay"></div>
+		</div>
+		<div class="choose animate-box" id="contact">
+			<h2>Contact</h2>
+				<div class="row form-group">
+					<div class="col-md-6">
+						<input type="text" id="fname" class="form-control" placeholder="Your firstname">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-md-6">
+						<input type="text" id="lname" class="form-control" placeholder="Your lastname">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="text" id="email" class="form-control" placeholder="Your email address">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="text" id="mobile" class="form-control" placeholder="Your mobile number">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Message"></textarea>
+					</div>
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary" onclick="sendMsg()">Send message</button>
+				</div>
+		</div>
+	</div>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src="js/jquery.easypiechart.min.js"></script>
+	
+	<!-- Main -->
+	<script src="js/main.js"></script>
+    <script>
+        function sendMsg()
+        {
+            var fname=$('#fname').val();
+            var lname=$('#lname').val();
+            var email=$('#email').val();
+            var mobile=$('#mobile').val();
+            var message=$('#message').val();
+            $.get("./send.php?value1="+fname+' '+lname+"&&value2="+email+' '+mobile+"&&value3="+message, function(data){
+            });
+            $('input').val('');
+            $('textarea').val('');
+
+        }
+    </script>
+	</body>
+</html>
+
